2f6084301d48b0f608e9073fa8a66478aeeeeb40
Zachary3352 <thelexingtonian1@gmail.com>
2020-03-07 13:07:49 -0500

    Finished get_tweets
    About to begin working on analyze_tweets.

--- MP3.py
+++ MP3.py
@@ -12,21 +12,25 @@ def get_tweets(screen_name):
     user = api.GetUser(screen_name=screen_name)
 
     all_tweets = open("alltweets.txt", "r+")
+    retrieval_date = all_tweets.readline().split("\n")[0]
 
-    print(all_tweets.readline(), "\n", date.today(), "\n", all_tweets.readline(), "\n", str(date.today()-timedelta(days=1)).split(" ")[0], "\n", all_tweets.readline(), "\n", str(date.today()-timedelta(days=2)).split(" ")[0])
+    if str(retrieval_date) == str(date.today()) or retrieval_date == str(date.today()-timedelta(days=1)).split(" ")[0] or retrieval_date == str(date.today()-timedelta(days=2)).split(" ")[0]:
+        print("Not refreshing")
+        return all_tweets
+    else:
+        print("Refreshing")
+        all_tweets.write(str(date.today())) # Put date of tweet retrieval at top of file.
+        all_tweets.write("\n\n")
+        for tweet in timeline:
+            all_tweets.write(str(tweet.full_text.encode("utf8")))
+            all_tweets.write("\n")
+        return all_tweets
 
-    # if all_tweets.readline() == date.today() or all_tweets.readline() == str(date.today()-timedelta(days=1)).split(" ")[0] or all_tweets.readline() == str(date.today()-timedelta(days=2)).split(" ")[0]:
-    #     print("Not refreshing")
-    #     return all_tweets
-    # else:
-    #     print("Refreshing")
-    #     all_tweets.write(str(date.today())) # Put date of tweet retrieval at top of file.
-    #     all_tweets.write("\n\n")
-    #     for tweet in timeline:
-    #         all_tweets.write(str(tweet.full_text.encode("utf8")))
-    #         all_tweets.write("\n")
-    #     return all_tweets
+
+def analyze_tweets(tweetsfile):
+
 
 get_tweets("zachary3352")
+analyze_tweets("alltweets.txt")
 
 #print("You've liked", user.favourites_count, "tweets.")
